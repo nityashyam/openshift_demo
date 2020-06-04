5b2ae40ead884018f8db43ddb3be6b3d05bc242e
Nitya Shyamsundar-DXC <nitya.shyamsundar-dxc@hpe.com>
2020-06-04 21:53:55 +0530

    Openshift

---
+++ SecondService/src/main/java/com/firstservice/demo/controller/ProductController.java
@@ -0,0 +1,15 @@
+package com.firstservice.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/consumer")
+public class ProductController {
+
+    @GetMapping("/message")
+    public String test() {
+        return "Hello JavaInUse Called in Second Service";
+    }
+}
