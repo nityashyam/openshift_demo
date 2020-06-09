3b5273be7fd7e6eea20aafa1e1ff36427c8e5ee2
Nitya Shyamsundar-DXC <nitya.shyamsundar-dxc@hpe.com>
2020-06-09 16:58:13 +0530

    test

--- rest_example/src/main/java/com/workflow/demo/MainApplication.java
+++ rest_example/src/main/java/com/workflow/demo/MainApplication.java
@@ -2,8 +2,6 @@ package com.workflow.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 public class MainApplication {
